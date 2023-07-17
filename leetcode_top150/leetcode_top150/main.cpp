af57156b7e8cdb1cacedc4f6d7f00cf86ef9b8bc
gaejuk <jejuvita01@gmail.com>
2023-07-17 10:34:16 -0700

    init

---
+++ leetcode_top150/leetcode_top150/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode_top150
+//
+//  Created by 김서현 on 7/17/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
