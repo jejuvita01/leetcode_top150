10545018e114e8a9052c497a607db35dac9911c3
gaejuk <jejuvita01@gmail.com>
2023-07-20 21:43:08 -0700

    1. Two Sum

---
+++ leetcode_top150/1_Two_Sum.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  leetcode_top150
+//
+//  Created by 김서현 on 7/20/23.
+//
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> answer;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    answer.push_back(i);
+                    answer.push_back(j);
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+};
